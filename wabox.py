fcc5a181a120e8ef1762d69a66bc12afe95b20e9
fadliselaz <fadliselaz@gmail.com>
2019-03-23 02:36:21 +0700

    alianz

--- wabox.py
+++ wabox.py
@@ -9,24 +9,29 @@ token = "1f413e0f2868ed11a1ad5c2472a2a1c85c7b88ab1f337"
 contact = sl
 rg = len(contact)
 msg = """
-*Visit labuan Bajo 2019*
+*Tahukah Anda?*
 
-Destinasi Favorit 
-turis lokal dan mancanegara.
+1. Bahwa biaya perawatan kamar (saja) di Rumah Sakit Singapore bisa mencapai *$688 per- malamnya?* Dan itu setara dengan *Rp.7,2 Juta??*
 
-Surga di Timur Indonesia,
-kunjungi Web Resmi Kami
-dibawah ini..
+2. Bahwa Allianz menanggung semua biaya perawatan, obat, operasi, donor transplantasi, pendamping, dll, sesuai tagihan dengan limit hingga 25 M??
 
-_Otoritas Pariwisata Labuan Bajo_
+3. Bahwa dengan setoran FLAT Premi Rupiah, Allianz akan menanggung biaya Rumah sakit dalam bentuk Dollar hingga ke seluruh dunia?
+
+*Yuni Pratiwi*
+Financial Advisor
+081311009769
+Allianz Life Indonesia
+_Happily Helping Others_
 
 """
 
+
+#Message saja
 for i in range(rg):
     uid = str(uuid.uuid1())
     data = urllib.urlencode({
         "token": token,
-        "uid": "6281213655573",
+        "uid": "6281932006544",
         "to": "62" + contact[i],
         "custom_uid": uid,
         "text": msg
@@ -38,19 +43,16 @@ for i in range(rg):
 
     print("succes send to {}".format(contact[i]))
 
-
-for i in range(rg):
     uid = str(uuid.uuid1())
     data = urllib.urlencode({
         "token": token,
-        "uid": "6281213655573",
+        "uid": "6281932006544",
         "to": "62" + contact[i],
         "custom_uid": uid,
-        "url": "https://www.indonesia.travel/gb/en/destinations/bali-nusa-tenggara/labuan-bajo",
-        "description" : "Destinasi Wisata di Timur Indonesia",
-        "caption" : "Visit Labuan Bajo 2019",
-        "url_thumb": "https://www.indonesia.travel/content/dam/indtravelrevamp/en/destinations/bali-nusa-tenggara/east-nusa-tenggara/labuan-bajo/LB1.jpg",
-      
+        "url": "https://gdurl.com/ZUHC/download",
+        "description" : "Allianz Life Indonesia - Happily Helping Others",
+        "caption" : "Tahukah Anda ?",
+        "url_thumb" : "https://gdurl.com/Jt49",
     })
 
     req = urllib2.Request('https://www.waboxapp.com/api/send/link', data)
@@ -58,3 +60,39 @@ for i in range(rg):
     result = response.read()
 
     print("succes send to {}".format("62" + contact[i]))
+
+
+# image with link and description
+# for i in range(rg):
+#     uid = str(uuid.uuid1())
+#     data = urllib.urlencode({
+#         "token": token,
+#         "uid": "6281932006544",
+#         "to": "62" + contact[i],
+#         "custom_uid": uid,
+#         "url": "https://www.indonesia.travel/gb/en/destinations/bali-nusa-tenggara/labuan-bajo",
+#         "description" : "Destinasi Wisata di Timur Indonesia",
+#         "caption" : "Visit Labuan Bajo 2019",
+#         "url_thumb": "https://www.indonesia.travel/content/dam/indtravelrevamp/en/destinations/bali-nusa-tenggara/east-nusa-tenggara/labuan-bajo/LB1.jpg",
+      
+#     })
+
+# any file
+# for i in range(rg):
+#     uid = str(uuid.uuid1())
+#     data = urllib.urlencode({
+#         "token": token,
+#         "uid": "6281932006544",
+#         "to": "62" + contact[i],
+#         "custom_uid": uid,
+#         "url": "https://gdurl.com/ZUHC/download",
+#         "description" : "Allianz Life Indonesia - Happily Helping Others",
+#         "caption" : "Tahukah Kamu ?",
+#         "url_thumb" : "https://gdurl.com/Jt49",
+#     })
+
+#     req = urllib2.Request('https://www.waboxapp.com/api/send/link', data)
+#     response = urllib2.urlopen(req)
+#     result = response.read()
+
+#     print("succes send to {}".format("62" + contact[i]))
